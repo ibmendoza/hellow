b9de3c3c41abfe00d0f143e45e7fa03c476d3800
Isagani Mendoza <gani.mendoza@gmail.com>
2023-05-12 22:14:37 +0800

    Update hello.go

--- hello/hello.go
+++ hello/hello.go
@@ -75,27 +75,3 @@ func World(ctx context.Context, name string) (*Response, error) {
 type Response struct {
 	Message string
 }
-
-// ==================================================================
-
-// Encore comes with a built-in development dashboard for
-// exploring your API, viewing documentation, debugging with
-// distributed tracing, and more. Visit your API URL in the browser:
-//
-//     http://localhost:4000
-//
-
-// ==================================================================
-
-// Next steps
-//
-// 1. Deploy your application to the cloud with a single command:
-//
-//     git push encore
-//
-// 2. To continue exploring Encore, check out one of these topics:
-//
-//    Building a Slack bot:  https://encore.dev/docs/tutorials/slack-bot
-//    Building a REST API:   https://encore.dev/docs/tutorials/rest-api
-//    Using SQL databases:   https://encore.dev/docs/develop/databases
-//    Authenticating users:  https://encore.dev/docs/develop/auth
